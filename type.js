33cf64c6948a231dadf11883471bff5a6679ee1e
Sourabh chetan <sourabhchetan1605@hotmail.com>
2025-08-02 07:21:28 +0530

    Update type.js

--- type.js
+++ type.js
@@ -1,7 +1,8 @@
 let typed = new Typed(".text", {
-    strings: ["I'm Tanbi Ghosh"],
+    strings: ["I'm sourabh chetan"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
+
 });
